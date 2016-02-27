63966fd42cec90a03bc20f7059f17015745d4ebb
Jakub Brzegowski <symfiz@gmail.com>
2016-02-27 20:12:39 +0100

    Ended class 1, started class 2 plus added one small program to test SWI Prolog Editor.

---
+++ miscellanea/swi_prolog_editor_test.pl
@@ -0,0 +1,5 @@
+% Author:
+% Date: 2016-02-22
+
+new(P, point(10,20)).
+new(@demo, dialog(’Demo Window’)).
---
+++ university classes/class_1.pl
@@ -0,0 +1,68 @@
+rodzic(kasia,robert).
+rodzic(tomek,robert).
+rodzic(tomek,eliza).
+rodzic(robert,anna).
+rodzic(robert,magda).
+rodzic(magda,jan).
+
+kobieta(kasia).
+kobieta(eliza).
+kobieta(magda).
+kobieta(anna).
+
+mezczyzna(tomek).
+mezczyzna(robert).
+mezczyzna(jan).
+
+% Opisanie zaleznosci rodzinnych:
+
+matka(X,Y) :-
+	rodzic(X,Y),
+	kobieta(X).
+
+ojciec(X,Y) :-
+	rodzic(X,Y),
+	mezczyzna(X).
+
+% Dalsze zaleznosci rodzinne:
+
+brat(X,Y) :-
+	rodzic(R,X),
+	rodzic(R,Y),
+	mezczyzna(X),
+	X \= Y.
+
+siostra(X,Y) :-
+	rodzic(R,X),
+	rodzic(R,Y),
+	kobieta(X),
+	X \= Y.
+
+dziadek(X,Y) :-
+	mezczyzna(X),
+	rodzic(X,Z),
+	rodzic(Z,Y).
+
+babcia(X,Y) :-
+	kobieta(X),
+	rodzic(X,Z),
+	rodzic(Z,Y).
+
+
+
+% Dalsze zaleznosci rodzinne
+
+przodek(X,Y) :-
+	rodzic(X,Y).
+
+przodek(X,Z) :-
+	rodzic(X,Y),
+	przodek(Y,Z).
+
+
+potomek(X,Y) :-
+	rodzic(Y,X).
+
+potomek(Z,X) :-
+	rodzic(Y,Z),
+	potomek(Y,X).
