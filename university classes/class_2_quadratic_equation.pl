10138e6049dc48739f49474af051c1e16d3ea349
Jakub Brzegowski <symfiz@gmail.com>
2016-02-29 12:23:00 +0100

    Added another implementation of predicat computing roots of quadratic equation and started implementation of 4-coloured map problem.

--- university classes/class_2_quadratic_equation.pl
+++ university classes/class_2_quadratic_equation.pl
@@ -16,3 +16,15 @@ delta_less_than_zero_msg(Delta) :-
   \+ Delta < 0.   % it can't end with ", fail.", because
                   % then the get_qe_roots will end before
                   % computing roots.
+
+% getting quadrating equation roots written
+% with only two predicates.
+get_qe_roots(A, B, C, Root) :-
+  delta(A, B, C, Delta),
+  \+ Delta < 0,
+  Root is (-B + sqrt(Delta)) / (2 * A).
+
+get_qe_roots(A, B, C, Root) :-
+  delta(A, B, C, Delta),
+  \+ Delta = 0,
+  Root is (-B - sqrt(Delta)) / (2 * A).
---
+++ university classes/class_2_simple_map_coloring.pl
@@ -0,0 +1,8 @@
+% we have map:
+%                     | Belarus
+%                     |------------
+%     Poland          |
+% --------------------|
+%           |         | Ukraine
+% Czech Rep.| Slovakia|-----------
+% --------------------
