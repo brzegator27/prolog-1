06e31343dfe99da2e07bb9ee0d4863111da91be0
Jakub Brzegowski <symfiz@gmail.com>
2016-03-02 17:25:40 +0100

    Solved problem of colouring simple map.

--- university classes/class_2_simple_map_coloring.pl
+++ university classes/class_2_simple_map_coloring.pl
@@ -6,3 +6,21 @@
 %           |         | Ukraine
 % Czech Rep.| Slovakia|-----------
 % --------------------
+
+colour(red).
+colour(green).
+colour(blue).
+
+colour_map(Poland, Belarus, Ukraine, Slovakia, CzechRep) :-
+  colour(Poland),       % colours must come first!
+  colour(Belarus),
+  colour(Ukraine),
+  colour(Slovakia),
+  colour(CzechRep),
+  Poland \= Belarus,    % then we should state other conditions
+  Poland \= Ukraine,
+  Poland \= CzechRep,
+  Poland \= Slovakia,
+  Belarus \= Ukraine,
+  Ukraine \= Slovakia,
+  Slovakia \= CzechRep.
